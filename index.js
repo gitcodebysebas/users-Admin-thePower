1d305e965bfd76be6d39832ec2f827fc228fbddf
Juan Pineda <juanpinedasebastian888@gmail.com>
2026-09-08 22:24:51 +0200

    agrego app.use que retorna un status

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
 const app = express();
+
+app.use("*", (req, res, next) => {
+    return res.status(404).json("Route not found");
+});
+
 app.listen(3000, () => {
     console.log("http://localhost:3000");
 });
